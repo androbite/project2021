7b66c7adeb0d479e1f2450470f8bdbc855878365
ujang <49551803+androbite@users.noreply.github.com>
2021-01-07 10:32:55 +0700

    kodingan fuad kepanjangan
    yang sedang-sedang saja

--- helloworld.java
+++ helloworld.java
@@ -4,15 +4,7 @@ public class HelloWorld {
         System.out.println("Selamat Datang Di Pondok Daqu");  
        
         String nama ="Fuad Hiadayat Asy-Syaibani";
-        int umur = 19, tahun =2020;
-        double Berat_Badan = 45.7;
-        String grade ="A";
-        
-        System.out.println("Nama : " + nama + ". umur :" + umur + "tahun "+",  Berat Badan :" + Berat_Badan + " grade :" + grade);
-        
-   
-    
-   
+        int umur = 19, tahun =2020;   
 }
 	
 }
