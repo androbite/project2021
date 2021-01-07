16cf79527230e51b1955e57be18010015c470fd1
fuad002 <77085017+fuad002@users.noreply.github.com>
2021-01-07 10:29:53 +0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,21 @@
 public class HelloWorld {
   public static void main(){
-    System.out.println("Hello World");
+    System.out.println("Hello Teman");
+        System.out.println("Selamat Datang Di Pondok Daqu");  
+       
+        String nama ="Fuad Hiadayat Asy-Syaibani";
+        int umur = 19, tahun =2020;
+        double Berat_Badan = 45.7;
+        String grade ="A";
+        
+        System.out.println("Nama : " + nama + ". umur :" + umur + "tahun "+",  Berat Badan :" + Berat_Badan + " grade :" + grade);
+        
+   
+    
+   
+}
+	
+}
+   
   }
 }
