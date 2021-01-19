45e816e9865d5cd5076a3b752dd1da4feae732eb
irpankhoiruljabar <77085160+irpankhoiruljabar@users.noreply.github.com>
2021-01-19 09:01:58 +0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
   public static void main(){
-    System.out.println("Hello Teman");
-        System.out.println("Selamat Datang Di Pondok Daqu");  
+    System.out.println("Assalamualaikum");
+        System.out.println("Selamat Datang Di chanel kami");  
        
-        String nama ="Fuad Hiadayat Asy-Syaibani";
-        int umur = 19, tahun =2020;   
+         System.out.println("Hello World"); 
+        System.out.println("Selamat " + "pagi");
 	}	
      }
