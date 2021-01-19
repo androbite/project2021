67d2e506fc2c33a14fb9cdec1f997a4f72d44226
irpankhoiruljabar <77085160+irpankhoiruljabar@users.noreply.github.com>
2021-01-19 09:05:53 +0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,24 @@
-public class HelloWorld {
-  public static void main(){
-    System.out.println("Assalamualaikum");
-        System.out.println("Selamat Datang Di chanel kami");  
+public static void main(String[] args) {
+      
+        
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 5;
+        
+        int jumlah = a + b + c + d;
+        
+        System.out.print("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d + ", total = ");
+        System.out.println(jumlah);
        
-         System.out.println("Hello World"); 
-        System.out.println("Selamat " + "pagi");
-	}	
-     }
+        System.out.println("a + c = " + a + c );
+        System.out.println("a + c = " + ( a + c) );
+    
+        System.out.println("b * d = " + (b * d) );
+
+        System.out.println("d / a = " + (d / a) );
+
+        
+    }   
+    
+}
